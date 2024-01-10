c7160c307cbc4cce26f7d13d46d1cc94abe80081
Gento91 <leo.kontente@gmail.com>
2024-01-10 14:39:03 +0100

    début menu

---
+++ scene/EurydiceIcone.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var screenSize = get_viewport_rect()
+	print(screenSize)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
